37b7058f709c9f26d519641bef29ea3378c53a0b
TWang <wang.tw.ted@gmail.com>
2024-08-19 17:37:35 +0000

    fix: action nodes

---
+++ src/basic_function/src/action_client.cpp
@@ -0,0 +1,121 @@
+#include <functional>
+#include <future>
+#include <memory>
+#include <string>
+#include <sstream>
+
+#include "basic_function/action/example.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+
+class ExampleActionClient : public rclcpp::Node
+{
+public:
+  using Example = basic_function::action::Example;
+  using GoalHandleExample = rclcpp_action::ClientGoalHandle<Example>;
+
+  explicit ExampleActionClient(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+  : Node("example_action_client", options)
+  {
+    this->client_ptr_ = rclcpp_action::create_client<Example>(
+      this,
+      "example");
+
+    this->timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(500),
+      std::bind(&ExampleActionClient::send_goal, this));
+  }
+
+  void send_goal()
+  {
+    // rclcpp::spin_some(this->get_node_base_interface());
+    using namespace std::placeholders;
+
+    this->timer_->cancel();
+
+    if (!this->client_ptr_->wait_for_action_server()) {
+      RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+      rclcpp::shutdown();
+    }
+
+    auto goal_msg = Example::Goal();
+    goal_msg.order = 10;
+
+    RCLCPP_INFO(this->get_logger(), "Sending goal");
+
+    auto send_goal_options = rclcpp_action::Client<Example>::SendGoalOptions();
+    send_goal_options.goal_response_callback =
+      std::bind(&ExampleActionClient::goal_response_callback, this, _1);
+    send_goal_options.feedback_callback =
+      std::bind(&ExampleActionClient::feedback_callback, this, _1, _2);
+    send_goal_options.result_callback =
+      std::bind(&ExampleActionClient::result_callback, this, _1);
+    this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+  }
+
+private:
+  rclcpp_action::Client<Example>::SharedPtr client_ptr_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  void goal_response_callback(std::shared_future<GoalHandleExample::SharedPtr> future)
+  {
+    auto goal_handle = future.get();
+    if (!goal_handle) {
+      RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+    } else {
+      RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+    }
+  }
+
+  void feedback_callback(
+    GoalHandleExample::SharedPtr,
+    const std::shared_ptr<const Example::Feedback> feedback)
+  {
+    std::stringstream ss;
+    ss << "Next number in sequence received: ";
+    for (auto number : feedback->partial_sequence) {
+      ss << number << " ";
+    }
+    RCLCPP_INFO(this->get_logger(), ss.str().c_str());
+  }
+
+  void result_callback(const GoalHandleExample::WrappedResult & result)
+  {
+    switch (result.code) {
+      case rclcpp_action::ResultCode::SUCCEEDED:
+        break;
+      case rclcpp_action::ResultCode::ABORTED:
+        RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+        return;
+      case rclcpp_action::ResultCode::CANCELED:
+        RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+        return;
+      default:
+        RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+        return;
+    }
+    std::stringstream ss;
+    ss << "Result received: ";
+    for (auto number : result.result->sequence) {
+      ss << number << " ";
+    }
+    RCLCPP_INFO(this->get_logger(), ss.str().c_str());
+    rclcpp::shutdown();
+  }
+};  // class ExampleActionClient
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto action_server = std::make_shared<ExampleActionClient>();
+
+  action_server->send_goal();
+
+  rclcpp::spin(action_server);
+  
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/basic_function/src/action_server.cpp
@@ -0,0 +1,107 @@
+#include <functional>
+#include <memory>
+#include <thread>
+
+#include "basic_function/action/example.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+
+class ExampleActionServer : public rclcpp::Node
+{
+public:
+  using Example = basic_function::action::Example;
+  using GoalHandleExample = rclcpp_action::ServerGoalHandle<Example>;
+
+  // ACTION_TUTORIALS_CPP_PUBLIC
+  explicit ExampleActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+  : Node("example_action_server", options)
+  {
+    using namespace std::placeholders;
+
+    this->action_server_ = rclcpp_action::create_server<Example>(
+      this->get_node_base_interface(),
+      this->get_node_clock_interface(),
+      this->get_node_logging_interface(),
+      this->get_node_waitables_interface(),
+      "example",
+      std::bind(&ExampleActionServer::handle_goal, this, _1, _2),
+      std::bind(&ExampleActionServer::handle_cancel, this, _1),
+      std::bind(&ExampleActionServer::handle_accepted, this, _1));
+  }
+
+private:
+  rclcpp_action::Server<Example>::SharedPtr action_server_;
+
+  rclcpp_action::GoalResponse handle_goal(
+    const rclcpp_action::GoalUUID & uuid,
+    std::shared_ptr<const Example::Goal> goal)
+  {
+    RCLCPP_INFO(this->get_logger(), "Received goal request with order %d", goal->order);
+    (void)uuid;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse handle_cancel(
+    const std::shared_ptr<GoalHandleExample> goal_handle)
+  {
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+  }
+
+  void handle_accepted(const std::shared_ptr<GoalHandleExample> goal_handle)
+  {
+    using namespace std::placeholders;
+    // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+    std::thread{std::bind(&ExampleActionServer::execute, this, _1), goal_handle}.detach();
+  }
+
+  void execute(const std::shared_ptr<GoalHandleExample> goal_handle)
+  {
+    RCLCPP_INFO(this->get_logger(), "Executing goal");
+    rclcpp::Rate loop_rate(1);
+    const auto goal = goal_handle->get_goal();
+    auto feedback = std::make_shared<Example::Feedback>();
+    auto & sequence = feedback->partial_sequence;
+    sequence.push_back(0);
+    sequence.push_back(1);
+    auto result = std::make_shared<Example::Result>();
+
+    for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
+      // Check if there is a cancel request
+      if (goal_handle->is_canceling()) {
+        result->sequence = sequence;
+        goal_handle->canceled(result);
+        RCLCPP_INFO(this->get_logger(), "Goal canceled");
+        return;
+      }
+      // Update sequence
+      sequence.push_back(sequence[i] + sequence[i - 1]);
+      // Publish feedback
+      goal_handle->publish_feedback(feedback);
+      RCLCPP_INFO(this->get_logger(), "Publish feedback");
+
+      loop_rate.sleep();
+    }
+
+    // Check if goal is done
+    if (rclcpp::ok()) {
+      result->sequence = sequence;
+      goal_handle->succeed(result);
+      RCLCPP_INFO(this->get_logger(), "Goal succeeded");
+    }
+  }
+};  // class ExampleActionServer
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc, argv);
+
+    auto action_server = std::make_shared<ExampleActionServer>();
+
+    rclcpp::spin(action_server);
+
+    rclcpp::shutdown();
+    return 0;
+}
